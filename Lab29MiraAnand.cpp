e4a4023c2b860a23373c99b3e00e8f5148112f6c
Mira Anand <miraanand@comcast.net>
2024-11-05 19:04:38 -0800

    Initial commit - Starting pseudocode

---
+++ Lab29MiraAnand.cpp
@@ -0,0 +1,4 @@
+// include my program header here
+// COMSC-210 | Lab 29 | Mira Anand
+// Module 11, Lesson: Software Development Life Cycle, Assignment: Project Proposal, Pseudocode, & Mockup
+// IDE used: Visual Studio Code for Mac
