d6fca9e09af0022f21ce64af713465b29deca5ef
Mira Anand <miraanand@comcast.net>
2024-11-05 19:15:09 -0800

    10 minute update - 1

--- Lab29MiraAnand.cpp
+++ Lab29MiraAnand.cpp
@@ -2,3 +2,21 @@
 // COMSC-210 | Lab 29 | Mira Anand
 // Module 11, Lesson: Software Development Life Cycle, Assignment: Project Proposal, Pseudocode, & Mockup
 // IDE used: Visual Studio Code for Mac
+
+// include the necessary headers here
+// define namespace (using namespace std;) after including the headers
+
+// include any global const variables here
+    // so far, for mockup/wireframe purposes, I am planning on creating a global const dummy variable to hold a product's name
+
+// define function prototypes here
+    // planning on having 3 functions
+    // 1. to output the contents of the inventory initially and then after each time period occurs
+    // 2. to perform the actual inventory simulation
+    // 3. a helper function to assist the inventory simulation function (so the inventory simulation function isn't too complex/cluttered)
+
+// int main() function starts here
+    // srand(time(0)); needed as first line in main() for randomization - so inventory simulation is more realistic
+
+    // creation of the foundational data structure
+    // I need to include detailed comments that explain this data structure piece by piece
